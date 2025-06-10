5ccd1366a53e2c914e69f4bef4121650e41be25f
Manish Jangid <manishjangid2005@gmail.com>
2025-06-10 13:47:24 +0530

    Initial commit

---
+++ src/data/config.ts
@@ -0,0 +1,41 @@
+const config = {
+  title: "Manish Jangid | Full-Stack Developer",
+  description: {
+    long: "Explore the portfolio of Manish, a full-stack developer and creative technologist specializing in interactive web experiences, 3D animations, and innovative projects. Discover my latest work, including Coding Ducks, The Booking Desk, Ghostchat, and more. Let's build something amazing together!",
+    short:
+      "Discover the portfolio of Manish, a full-stack developer creating interactive web experiences and innovative projects.",
+  },
+  keywords: [
+    "Manish",
+    "portfolio",
+    "full-stack developer",
+    "creative technologist",
+    "web development",
+    "3D animations",
+    "interactive websites",
+    "Coding Ducks",
+    "The Booking Desk",
+    "Ghostchat",
+    "web design",
+    "GSAP",
+    "React",
+    "Next.js",
+    "Spline",
+    "Framer Motion",
+  ],
+  author: "Manish Jangid",
+  email: "20005manishjangid@gmail.com",
+  site: "https://google.com",
+
+  get ogImg() {
+    return this.site + "/assets/seo/og-image.png";
+  },
+  social: {
+    twitter: "https://x.com/nothotchaddi",
+    linkedin: "https://www.linkedin.com/in/manishjangid20005",
+    instagram: "https://www.instagram.com/hotchaddi",
+    facebook: "https://www.facebook.com/HotChaddi/",
+    github: "https://github.com/manishjangid20005",
+  },
+};
+export { config };
